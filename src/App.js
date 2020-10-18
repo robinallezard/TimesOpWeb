e62c6b8a83a2c1ab4df2bde7cc260b3e5ed13bac
robinallezard <robinallezard@gmail.com>
2020-10-18 11:42:35 +0200

    build 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StateProvider } from './store.js';
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Home from './Components/Home.js';
 import Jeu from './Components/Jeu.js';
 import Win from './Components/Win.js';
@@ -12,7 +12,7 @@ function App() {
     return (
         <StateProvider>
         <div className="min-h-screen bg-gray-300 text-gray-900 font-sans flex justify-center items-center flex-col p-5 border-box">
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router>
             <Switch>
                 <Route path="/jeu">
                     <Jeu/>
